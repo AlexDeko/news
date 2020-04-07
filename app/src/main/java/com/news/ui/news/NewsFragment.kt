0991bbb71deb378af16cdc607f5063feaa8ad2eb
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-04-07 22:12:14 +0300

    Initial commit

---
+++ app/src/main/java/com/news/ui/news/NewsFragment.kt
@@ -0,0 +1,31 @@
+package com.news.ui.news
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.news.R
+
+class NewsFragment : Fragment() {
+
+    private lateinit var homeViewModel: NewsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProviders.of(this).get(NewsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_new, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/news/ui/popular/PopularFragment.kt
@@ -0,0 +1,31 @@
+package com.news.ui.popular
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.news.R
+
+class PopularFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: PopularViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(PopularViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_popular, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
