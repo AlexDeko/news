9ea4ee330cc2b4eb5669a72dfaccd7b24c08c38a
Alexandr Deynenko <alex.deko.69@gmail.com>
2020-04-08 11:35:19 +0300

    add attribute for fragment

--- app/src/main/java/com/news/ui/MainActivity.kt
+++ app/src/main/java/com/news/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.news
+package com.news.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.news.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +20,9 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_news, R.id.navigation_popular))
+            R.id.navigation_news,
+            R.id.navigation_popular
+        ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
